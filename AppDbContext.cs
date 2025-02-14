4ef214041dae0103bbe575af49a9dad3f439d9d1
joaowozniack <90704865+joaowozniack@users.noreply.github.com>
2025-02-14 15:08:13 -0300

    Configuracao do Context

---
+++ AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProtocolSystem.Models;
+
+namespace ProtocolSystem
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Protocolo> Protocolos { get; set; }
+        public DbSet<ProtocoloFollow> ProtocoloFollows { get; set; }
+        public DbSet<StatusProtocolo> StatusProtocolos { get; set; }
+    }
+}
